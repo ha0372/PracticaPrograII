4cc8b3a7dde8207e5043bbae87dfc2374fa89b60
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-09-23 16:11:13 -0600

    ejercicioCore

---
+++ PracticaClassPrograII/PracticaClassPrograII/Controllers/RegistroController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaClassPrograII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaClassPrograII.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ClsValidacion validacion) 
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ PracticaClassPrograII/PracticaClassPrograII/Models/ClsValidacion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaClassPrograII.Models
+{
+    public class ClsValidacion
+    {
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+
+        public String Nombre { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+        public String Sexo { get; set; }
+
+        [Display(Name = "tipoEstudio")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+        public String tipoEstudio { get; set; }
+
+    }
+}
